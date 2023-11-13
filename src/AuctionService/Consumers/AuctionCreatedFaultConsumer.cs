f560538c79eeee4afc0ee39beda7bda6083ea3e1
Daniel Dalby <55078080+Dalle-valle@users.noreply.github.com>
2023-11-13 12:38:16 +0100

    end of section 4

---
+++ src/AuctionService/Consumers/AuctionCreatedFaultConsumer.cs
@@ -0,0 +1,27 @@
+﻿using MassTransit;
+using Contracts;
+
+namespace AuctionService;
+
+public class AuctionCreatedFaultConsumer : IConsumer<Fault<AuctionCreated>>
+{ 
+    public async Task Consume(ConsumeContext<Fault<AuctionCreated>> context)
+    {
+        Console.WriteLine("--> Consuming auction created fault");
+
+        var exception = context.Message.Exceptions.First();   
+        
+
+
+        if (exception.ExceptionType == "System.ArgmuentException")
+         {
+             context.Message.Message.Model = "FooBar";
+             await context.Publish(context.Message.Message);
+         } else 
+
+         {
+            Console.WriteLine("Not an argument Exception - Update error dashboard somewhere");
+         }
+
+}
+}
--- src/AuctionService/RequestHelpers/MappingProfiles.cs
+++ src/AuctionService/RequestHelpers/MappingProfiles.cs
@@ -1,18 +1,23 @@
 ﻿using AuctionService.Entities;
 using AuctionService.DTOs;
 using AutoMapper;
+using Contracts;
 namespace AuctionService.RequestHelpers;
 
 public class MappingProfiles : Profile
 {
     public MappingProfiles()
     {
+
+
         CreateMap<Auction, AuctionDto>().IncludeMembers(x => x.Item);
         CreateMap<Item, AuctionDto>();
-        CreateMap<AuctionDto, Auction>()
+       CreateMap<CreateAuctionDto, Auction>()
             .ForMember(d => d.Item, o => o.MapFrom(s => s));
-
-        CreateMap<AuctionDto, Item>();
+        CreateMap<CreateAuctionDto, Item>();
+        CreateMap<AuctionDto, AuctionCreated>();
+        CreateMap<Auction, AuctionUpdated>().IncludeMembers(a => a.Item);
+        CreateMap<Item, AuctionUpdated>();
     }
   
 }
